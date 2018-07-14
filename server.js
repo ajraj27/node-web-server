3636653d5011f5957bb67cbb485ed60e7353de6e
ajraj27 <rajanuj.iiita@gmail.com>
2018-07-15 03:28:06 +0530

    Commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const hbs=require('hbs');
 
+const port=process.env.PORT || 3000;
 var app=express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -30,4 +31,4 @@ app.get('/about',(req,res) => {
 
 
 
-app.listen(3002);
+app.listen(port);
